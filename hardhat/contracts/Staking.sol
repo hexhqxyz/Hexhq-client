63a26cd93115994585ff37f4881b7fe574a237b4
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-06 13:22:46 +0530

    rewards per token

--- hardhat/contracts/Staking.sol
+++ hardhat/contracts/Staking.sol
@@ -8,8 +8,24 @@ contract Staking is ReentrancyGuard {
     IERC20 public s_stakingToken;
     IERC20 public s_rewardToken;
 
+    uint public constant REWARD_RATE = 10;
+    uint private totalStakedToken;
+    uint public rewardPerTokenStored;
+    uint public lastUpdateTime;
+
     constructor(address stakingTokenAddress, address rewardTokenAddress) {
         s_stakingToken = IERC20(stakingTokenAddress);
         s_rewardToken = IERC20(rewardTokenAddress);
+    }
+
+    function rewardsPerToken() public view  returns (uint) {
+        if(totalStakedToken == 0){
+         return rewardPerTokenStored;
+        }
+
+        uint totalTime = block.timestamp - lastUpdateTime;
+        uint totalRewards = REWARD_RATE * totalTime;
+
+        return rewardPerTokenStored + totalRewards / totalStakedToken;
     }
 }
