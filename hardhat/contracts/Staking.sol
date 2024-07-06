2e465d72cb943c29a1fadb6f14f159c46174a12f
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-06 13:07:41 +0530

    constructor, token initialization

--- hardhat/contracts/Staking.sol
+++ hardhat/contracts/Staking.sol
@@ -5,5 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Staking is ReentrancyGuard {
-   
+    IERC20 public s_stakingToken;
+    IERC20 public s_rewardToken;
+
+    constructor(address stakingTokenAddress, address rewardTokenAddress) {
+        s_stakingToken = IERC20(stakingTokenAddress);
+        s_rewardToken = IERC20(rewardTokenAddress);
+    }
 }
