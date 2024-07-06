98d4cbfd1c67d334ec50ec2834830dd9fb91835d
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-06 12:57:27 +0530

    staking contract starts, importing reentrancy

---
+++ hardhat/contracts/Staking.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Staking is ReentrancyGuard {
+   
+}
