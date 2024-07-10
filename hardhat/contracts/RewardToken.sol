2102996b9a9ee3104f340cb44e54b1eccd0beae1
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-10 15:42:48 +0530

    tokens testing

--- hardhat/contracts/RewardToken.sol
+++ hardhat/contracts/RewardToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import "hardhat/console.sol";
 
 contract RewardToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("OmniDefi: Staking Rewards ", "dUSD") {
+    constructor(uint256 initialSupply) ERC20("OmniDefi: Staking Rewards", "dUSD") {
         _mint(msg.sender, initialSupply*10**18);
     }
 }
