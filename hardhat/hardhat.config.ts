965e384d467491fa93bdaab538d6dda308d55341
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-11 13:50:19 +0530

    hardhat chain id to 1337

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -9,16 +9,19 @@ const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  // etherscan: {
-  //   apiKey: ETHERSCAN_API_KEY,
-  // },
-  // networks: {
-  //   sepolia: {
-  //     url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-  //     accounts: [SEPOLIA_PRIVATE_KEY],
-  //   },
-  //   hardhat: {}
-  // },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    // sepolia: {
+    //   url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
