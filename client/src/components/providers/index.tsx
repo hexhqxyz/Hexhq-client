375f104a875713d6bcbb390d54f611eca83828db
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-13 15:35:06 +0530

    shadcn setup

---
+++ client/src/components/providers/index.tsx
@@ -0,0 +1,24 @@
+import { Web3Modal } from "@/context/web3modal";
+import React from "react";
+import { ThemeProvider } from "./theme-provider";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Providers = ({ children }: Props) => {
+  return (
+    <div>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Web3Modal>{children}</Web3Modal>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default Providers;
