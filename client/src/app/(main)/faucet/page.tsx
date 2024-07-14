b9976a854f748f5ecefcacbdbbb9434ad46231f8
Sachin Gurjar <sachinhlo232@gmail.com>
2024-07-14 13:21:35 +0530

    faucet page setup

---
+++ client/src/app/(main)/faucet/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>
+        Faucet..
+    </div>
+  )
+}
+
+export default Page
--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -5,22 +5,57 @@ import { Heading } from "../ui/Typography";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "../ui/button";
+import { Github } from "lucide-react";
 
 type Props = {};
+
+const links = [
+  {
+    href: "/staking",
+    label: "Staking",
+  },
+  {
+    href: "/lending",
+    label: "Lending",
+  },
+  {
+    href: "/amm",
+    label: "Amm",
+  },
+  {
+    href: "/yield-farming",
+    label: "Yield farming",
+  },
+  {
+    href: "/faucet",
+    label: "Faucet",
+  },
+];
 
 const Navbar = (props: Props) => {
   return (
     <div className="flex justify-between items-center">
       <Heading variant="h3">OmniDeFi</Heading>
       <div className="flex items-center gap-x-2">
-        <Link className={cn(buttonVariants({variant: "outline"}))} href={"/staking"}>Staking</Link>
-        <Link className={cn(buttonVariants({variant: "outline"}))} href={"/staking"}>Lending</Link>
-        <Link className={cn(buttonVariants({variant: "outline"}))} href={"/staking"}>AMM</Link>
-        <Link className={cn(buttonVariants({variant: "outline"}))} href={"/staking"}>Yield Farming</Link>
+        {links?.map((item, index) => (
+          <Link
+            key={index}
+            className={cn(buttonVariants({ variant: "outline" }))}
+            href={item.href}
+          >
+            {item.label}
+          </Link>
+        ))}
       </div>
       <div className="flex items-center gap-x-2">
-        <ThemeSwitcher />
         <ConnectButton />
+        <Link
+          href={"https://github.com/SachinCoder1/Omni-DeFi"}
+          target="_blank"
+        >
+          <Github className="w-5 h-5" />
+        </Link>
+        <ThemeSwitcher />
       </div>
     </div>
   );
