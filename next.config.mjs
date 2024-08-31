9314856683c6809661a25ef22c02f872e4693311
Sachin Gurjar <sachinhlo232@gmail.com>
2024-08-31 16:47:39 +0530

    nextjs export to out directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    exportPathMap: () => {
+        return {
+            "/": { page: "/" }
+        }
+    },
+};
 
 export default nextConfig;
